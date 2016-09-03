e9e81294956a6acdaa0af8569f700c6be0e30140
jmredondo1 <jmredondo1@gmail.com>
2016-09-03 18:56:27 +0200

    Add files via upload

---
+++ FicherosDirectorios.java
@@ -0,0 +1,33 @@
+// recorrido recursivo de directorios
+package ficherosdirectorios;
+
+import com.sun.org.apache.xml.internal.serializer.utils.SystemIDResolver;
+import java.io.File;
+
+public class FicherosDirectorios {
+
+    public static void main(String[] args) {
+
+        File f = new File("C:\\Users\\José María\\Desktop");
+
+        System.out.println(f.getAbsolutePath());
+        System.out.println(f.exists());
+
+        String[] lista = f.list();
+        for (String string : lista) {
+            
+            File f2 = new File(f, string);
+            if (!f2.isDirectory()) {
+                System.out.println(string);
+            }else{
+                System.out.println("-D-"+string);
+                String[] subcarpeta = f2.list();
+                for (String string1 : subcarpeta) {
+                    System.out.println(string1);
+                }
+            }
+
+        }
+    }
+
+}
